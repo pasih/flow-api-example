cbc54a4825dc609080e8d679ba10739fc16c6a13
Pasi Hirvonen <pasi.hirvonen@seepigames.com>
2017-04-18 22:58:21 +0300

    Code that kills flow

---
+++ src/foo.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const x = props => (
+  <div>
+    <h1>hei: {props.foo}</h1>
+  </div>
+);
+
+export default x;
--- src/index.js
+++ src/index.js
@@ -3,14 +3,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
-import App from "./App";
-
-import "./index.css";
 
 import { createStore, applyMiddleware, compose } from "redux";
 import type { Store } from "redux";
 import { validate } from "validated/object";
 import { arrayOf, object, string, number, boolean } from "validated/schema";
+
+import App from "./App";
+
+import "./index.css";
 
 type Todo = {
   userId: number,
@@ -26,13 +27,13 @@ type Album = {
 };
 
 /* Validation schemas for validating a server response payload */
-const albumSchema = object({
-  userId: number,
-  id: number,
-  title: string
-});
+// const albumSchema = object({
+//   userId: number,
+//   id: number,
+//   title: string
+// });
 
-const AlbumsSchema = arrayOf(albumSchema);
+// const AlbumsSchema = arrayOf(albumSchema);
 
 const todosSchema = arrayOf(
   object({
@@ -44,28 +45,57 @@ const todosSchema = arrayOf(
 );
 
 type State = {
-  todos: ?Array<Todo>,
-  albums: ?Array<Album>,
-  album: ?Album
-};
-
-type apiPayload = {
-  url: string,
-  next: (mixed) => Action
+  +todos: ?Array<Todo>,
+  +albums: ?Array<Album>,
+  +album: ?Album
 };
 
 /* In reality, these would probably be generic types with error and meta. I'm keeping it
    simple here. */
 
-type Action =
-  | { type: "API", payload: apiPayload }
-  | { type: "LOADED_TODOS", payload: Array<Todo> }
-  | { type: "LOADED_ALBUMS", payload: Array<Album> }
-  | { type: "LOADED_ALBUM", payload: Album }
-  | { type: "ALBUM_UPDATED", payload: boolean };
+// type ActionType =
+//   | "API"
+//   | "LOADED_TODOS"
+//   | "LOADED_ALBUMS"
+//   | "LOADED_ALBUM"
+//   | "ALBUM_UPDATED";
+//
+// type ActionT<A: ActionType, P> = {|
+//   type: A,
+//   payload?: P | Error,
+//   error?: boolean,
+//   meta?: mixed
+// |};
 
+// type ActionT = {
+//   type: string,
+//   payload?: mixed | Error,
+//   error?: boolean,
+//   meta?: mixed
+// };
+
+// type PayloadType = { todos: Array<Todo> } | apiPayload;
+type apiPayload = {
+  url: string,
+  next: Function
+};
+
+type ApiAction = { type: "API", payload: apiPayload };
+type LoadedTodosAction = { type: "LOADED_TODOS", payload: Array<Todo> };
+
+type Action = ApiAction | LoadedTodosAction;
+
+// type NextFunc = mixed => Action;
+
+// type Action =
+//   | { type: "API", payload: apiPayload }
+//   | { type: "LOADED_TODOS", payload: Array<Todo> }
+// | { type: "LOADED_ALBUMS", payload: Array<Album> }
+// | { type: "LOADED_ALBUM", payload: Album }
+// | { type: "ALBUM_UPDATED", payload: boolean };
+
+export type ThunkAction = (dispatch: Dispatch, getState: GetState) => any; // eslint-disable-line
 export type GetState = () => Object;
-export type ThunkAction = (dispatch: Dispatch, getState: GetState) => any;
 export type Dispatch = (action: Action | ThunkAction) => any;
 
 const INITIAL_STATE = {
@@ -74,18 +104,52 @@ const INITIAL_STATE = {
   album: undefined
 };
 
-const loadedTodos = (payload): Action => {
-  const validData: Array<Todo> = validate(todosSchema, payload);
-  return {
-    type: "LOADED_TODOS",
-    payload: validData
-  };
-};
+// const loadedTodos = (payload: mixed): LoadedTodosAction => {
+//   const validData: Array<Todo> = validate(todosSchema, payload);
+//   return {
+//     type: "LOADED_TODOS",
+//     payload: validData
+//   };
+// // };
+//
 
-// function createNextAction(type: string, schema: Node<*>): (mixed) => Action {
-//   return function<RT>(payload: mixed): Action {
+function mockValidate(schema, payload): Array<Todo> {
+  return [{ userId: 5, id: 5, title: "blaa", completed: true }];
+}
+
+function loadedTodos2<T, X>(s: string) {
+  return function foo(payload: mixed): T {
+    const validData: X = mockValidate(todosSchema, payload);
+    return {
+      type: s,
+      payload: X
+    };
+  };
+}
+
+// function myValidate<RT>(schema, payload: mixed): RT {
+//   const f: RT = validate(schema, payload);
+//   return f;
+// }
+
+// function createNextAction<P, T: Action>(
+//   type: string,
+//   schema: Node<*>
+// ): NextFunc {
+//   return function(payload: mixed): T {
 //     // This throws if validate() fails. Otherwise we get a "good" payload
-//     const validData: RT = validate(schema, payload);
+//     const validData: P = myValidate(schema, payload);
+//     return {
+//       type,
+//       validData
+//     };
+//   };
+// }
+
+// function createNextAction<T: Action>(type: Action, schema: Node<*>): MAG {
+//   return function(payload: mixed): T {
+//     // This throws if validate() fails. Otherwise we get a "good" payload
+//     const validData: PayloadType = validate(schema, payload);
 //     return {
 //       type,
 //       payload: validData
@@ -97,7 +161,7 @@ const getTodos = (): Action => ({
   type: "API",
   payload: {
     url: "todos",
-    next: loadedTodos
+    next: loadedTodos2("LOADED_TODOS")
   }
 });
 
@@ -158,27 +222,57 @@ const rootReducer = (state: State = INITIAL_STATE, action: Action) => {
 
 const BASE_URL = "https://jsonplaceholder.typicode.com/";
 
-const apiMiddleware = ({ dispatch }: { dispatch: Dispatch }) =>
-  next =>
-    action => {
-      if (action.type !== "API") {
-        return next(action);
+const response = {
+  json() {
+    console.log("json called");
+    return [
+      {
+        userId: 1,
+        id: 1,
+        title: "delectus aut autem",
+        completed: false
+      },
+      {
+        userId: 1,
+        id: 2,
+        title: "quis ut nam facilis et officia qui",
+        completed: false
       }
+    ];
+  }
+};
 
-      const { payload } = action;
+function mockFetch(url: string): Promise<*> {
+  console.log(`Fetching ${url}`);
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(response);
+    }, 2500);
+  });
+}
 
-      fetch(BASE_URL + payload.url)
-        .then(response => response.json())
-        .then(response => dispatch(payload.next(response)))
-        .catch(error => {
-          console.log("------- CAUGHT ERROR -------");
-          console.log(error);
-          /* In real app we would call payload.next with an error here */
-        });
-    };
+const apiMiddleware = ({
+  dispatch
+}: { dispatch: Dispatch }) => next => action => {
+  if (action.type !== "API") {
+    return next(action);
+  }
+
+  const { payload } = action;
+
+  mockFetch(BASE_URL + payload.url)
+    .then(r => r.json())
+    .then(r => dispatch(payload.next(r)))
+    .catch(error => {
+      console.log("------- CAUGHT ERROR -------");
+      console.log(error);
+      /* In real app we would call payload.next with an error here */
+    });
+  return next(action);
+};
 
 // const rootReducer = createReducer(INITIAL_STATE, );
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; //eslint-disable-line
 const store: Store = createStore(
   rootReducer,
   composeEnhancers(applyMiddleware(thunk, apiMiddleware))
